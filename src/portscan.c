7579d0371a862ad65c56818c6b34444efc9147d9
Sideway <dev.sideway@gmail.com>
2016-12-21 13:52:53 +0100

    Added basic argument handling

--- src/portscan.c
+++ src/portscan.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "utils.h"
 
-int main(){
+int main(int argc,char** argv)
+{
 	int i;
+	int maxport = 1024;
+	
+	if(argc < 2){
+		perror("Not enough arguments");
+		exit(-1);
+	}
 
-	for(i=0;i<1000;i++){
-		if(check("192.168.1.31",i) > 0){
+	if(argc > 2){
+		maxport = atoi(argv[2]);
+	}
+
+	for(i=0;i<maxport;i++){
+		if(check(argv[1],i) > 0){
 			printf("%d open\n",i);
 		}
 	}
